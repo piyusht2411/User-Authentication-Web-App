fd025197ab5d3f2fe66f6eb5e3385f16e889c8d3
Piyush <piyushthakur241199@gmail.com>
2022-08-19 19:23:43 +0530

    done with tem

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+app.use(express.urlencoded);
+app.use(cookieParser());
 const port = 8000;
 const favicon = require('serve-favicon');
 app.use(favicon(__dirname + '/images/favicon.ico'));
 
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
-app.set('views','./views')
+app.set('views', './views')
 
 
-app.listen(port, (err)=>{
-    if(err){ console.log(`Error is :${err}`);}
+app.listen(port, (err) => {
+    if (err) { console.log(`Error is :${err}`); }
     console.log(`Server is runing on port : ${port}`);
 })
