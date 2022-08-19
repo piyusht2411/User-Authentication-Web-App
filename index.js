13eaaaa413ddfb09e8800340f457e50ff2ee549a
Piyush <piyushthakur241199@gmail.com>
2022-08-19 15:30:16 +0530

    temp

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const favicon = require('serve-favicon');
+app.use(favicon(__dirname + '/images/favicon.ico'));
 
 app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views','./views')
+
 
 app.listen(port, (err)=>{
     if(err){ console.log(`Error is :${err}`);}
