025387b52ff67a14e1d86b5593b08a5928968d13
Piyush <piyushthakur241199@gmail.com>
2022-08-19 18:43:02 +0530

    ok

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,3 +3,14 @@ module.exports.profile = function(req,resp){
         title:"profile"
     });
 }
+
+module.exports.signup = function(req,resp){
+    resp.render('user_signUp', {
+        title: "Sign Up"
+    })
+}
+module.exports.signIn = function(req,resp){
+    resp.render('user_signIn', {
+        title: "Sign In"
+    })
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const user_contrller = require('../controllers/users.controller');
 
 router.get('/profile', user_contrller.profile);
+router.get('/sign-in', user_contrller.signIn);
+router.get('/sign-up', user_contrller.signup);
 
 
 module.exports = router;
