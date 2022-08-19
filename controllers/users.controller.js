b12325e35974f2ae9c9eaa6fdc50ded7a2f8b412
Piyush <piyushthakur241199@gmail.com>
2022-08-19 17:51:14 +0530

    untill cookie pakage

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -3,3 +3,21 @@ module.exports.profile = function(req,resp){
         title:"profile"
     });
 }
+
+module.exports.sign_in = function(req,resp){
+    resp.render('user_signIn', {
+        title:"Sign In"
+    })
+}
+module.exports.sign_up = function(req,resp){
+    resp.render('user_signup', {
+        title:"Sign Up"
+    })
+}
+
+module.exports.create = function(req,resp){
+
+}
+module.exports.createSession = function(req,resp){
+    
+}
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const user_contrller = require('../controllers/users.controller');
 
 router.get('/profile', user_contrller.profile);
+router.get('/sign-in', user_contrller.sign_in);
+router.get('/sign-up', user_contrller.sign_up);
 
 
 module.exports = router;
