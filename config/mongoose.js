7649385e593fba1c64fa2a9480d908dbe087c6bc
Piyush <piyushthakur241199@gmail.com>
2022-08-20 19:29:32 +0530

    done

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,7 +3,13 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/social-chat');
 
 const db = mongoose.connection;
-db.on('error', (err)=>{ console.log(err.message);})
-db.once('open ', function(){
-    console.log("Succesfully connected to the database");
-})
+
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
