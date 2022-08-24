9300dfbbf2da8361637a21bb85be0d7d335c9464
Piyush <piyushthakur241199@gmail.com>
2022-08-25 00:06:10 +0530

    8 digit password

--- models/user.js
+++ models/user.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:true,
+    password: {
+        type: String,
+        required: true,
+        min: 8,
+        max: 24
 
     },
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: Number,
+        required: true,
+        min: 100000000,
+        max: 999999999
+
     }
 }, {
-    timestamps:true
+    timestamps: true
 });
 
 const User = mongoose.model('user', userSchema);
